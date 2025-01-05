7890a90e150845bd159ef7a50c2a41d14aed7af0
lcaohoanq <hoangclw@gmail.com>
2025-01-05 22:58:32 +0700

    fix: fix unit test on ci

--- web_frameworks/SpringBoot/src/test/java/com/lcaohoanq/demo/UserControllerTest.java
+++ web_frameworks/SpringBoot/src/test/java/com/lcaohoanq/demo/UserControllerTest.java
@@ -10,10 +10,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 @SpringBootTest
+@ActiveProfiles("test")
 @AutoConfigureMockMvc
 public class UserControllerTest {
 
@@ -22,7 +24,7 @@ public class UserControllerTest {
 
     @Test
     public void getHello() throws Exception {
-        mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
+        mvc.perform(MockMvcRequestBuilders.get("/api/v1/users").accept(MediaType.APPLICATION_JSON))
             .andExpect(status().isOk())
             .andExpect(content().string(equalTo("Greetings from Spring Boot!")));
     }
